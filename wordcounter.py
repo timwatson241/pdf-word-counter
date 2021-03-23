1c16988db689058298f5851a6d51bc8587c48ef8
Timothy Watson <timothywatson@Timothys-MacBook-Pro.local>
2021-03-23 12:36:28 -0700

    commenting for clarification

--- wordcounter.py
+++ wordcounter.py
@@ -1,14 +1,18 @@
 from pdf2image import convert_from_path
 import pytesseract
 
+# location of tesseract executable
 pytesseract.pytesseract.tesseract_cmd = r'/opt/homebrew/bin/tesseract'
 
+# open pdf
 doc_name = 'Slim_shady.pdf'
 pages = convert_from_path('pdf_docs/'+doc_name, 500)
 
-page_number=0
-
+# word of interest
 word = 'Shady'
+
+# initiate counters
+page_number=0
 word_count=0
 
 for page in pages:
@@ -19,9 +23,10 @@ for page in pages:
     
     # Read words from jpeg
     string=pytesseract.image_to_string(page_name)
-    print(string)
     
+    #count word on the page and add result to counter
     word_count += string.upper().count(word.upper())
+    
     page_number+=1
 
 print('The word '+word+' appears '+ str(word_count)+' times')
